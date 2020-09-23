2712632c60f52bd8285dd31e47f24856c587defe
uqxtan9 <uqxtan9@wiener.hpc.dc.uq.edu.au>
2020-09-23 21:20:03 +1000

    test nb

---
+++ stimage/utils.py
@@ -0,0 +1,331 @@
+from matplotlib import pyplot as plt
+from PIL import Image
+import pandas as pd
+import matplotlib
+import numpy as np
+
+from stlearn._compat import Literal
+from typing import Optional, Union
+from anndata import AnnData
+import warnings
+#from .utils import get_img_from_fig, checkType
+
+
+def gene_plot(
+    adata: AnnData,
+    method: str = "CumSum",
+    genes: Optional[Union[str, list]] = None,
+    threshold: float = None,
+    library_id: str = None,
+    data_alpha: float = 1.0,
+    tissue_alpha: float = 1.0,
+    cmap: str = "Spectral_r",
+    spot_size: Union[float, int] = 6.5,
+    show_legend: bool = False,
+    show_color_bar: bool = True,
+    show_axis: bool = False,
+    cropped: bool = True,
+    margin: int = 100,
+    name: str = None,
+    output: str = None,
+    copy: bool = False,
+) -> Optional[AnnData]:
+    """\
+    Gene expression plot for sptial transcriptomics data.
+    Parameters
+    ----------
+    adata
+        Annotated data matrix.
+    library_id
+        Library id stored in AnnData.
+    method
+        Use method to count. We prorive: NaiveMean, NaiveSum, CumSum.
+    genes
+        Choose a gene or a list of genes.
+    threshold
+        Threshold to filter genes
+    data_alpha
+        Opacity of the spot.
+    tissue_alpha
+        Opacity of the tissue.
+    cmap
+        Color map to use.
+    spot_size
+        Size of the spot.
+    show_color_bar
+        Show color bar or not.
+    show_axis
+        Show axis or not.
+    show_legend
+        Show legend or not.
+    show_trajectory
+        Show the spatial trajectory or not. It requires stlearn.spatial.trajectory.pseudotimespace.
+    show_subcluster
+        Show subcluster or not. It requires stlearn.spatial.trajectory.global_level.
+    name
+        Name of the output figure file.
+    output
+        Save the figure as file or not.
+    copy
+        Return a copy instead of writing to adata.
+    Returns
+    -------
+    Nothing
+    """
+
+    #plt.rcParams['figure.dpi'] = dpi
+
+    if type(genes) == str:
+        genes = [genes]
+    colors = _gene_plot(adata, method, genes)
+
+    if threshold is not None:
+        colors = colors[colors>threshold]
+
+    index_filter = colors.index
+
+    filter_obs  = adata.obs.loc[index_filter]
+
+    imagecol = filter_obs["imagecol"]
+    imagerow = filter_obs["imagerow"]
+    
+    # Option for turning off showing figure
+    plt.ioff()
+
+    # Initialize matplotlib
+    fig, a = plt.subplots()
+
+    vmin = min(colors)
+    vmax = max(colors)
+    # Plot scatter plot based on pixel of spots
+    plot = a.scatter(imagecol, imagerow, edgecolor="none", alpha=data_alpha, s=spot_size, marker="o",
+                     vmin=vmin, vmax=vmax, cmap=plt.get_cmap(cmap), c=colors)
+
+    if show_color_bar:
+
+        cb = plt.colorbar(plot, cax=fig.add_axes(
+            [0.9, 0.3, 0.03, 0.38]), cmap=cmap)
+        cb.outline.set_visible(False)
+
+    if not show_axis:
+        a.axis('off')
+
+    if library_id is None:
+        library_id = list(adata.uns["spatial"].keys())[0]
+
+    image = adata.uns["spatial"][library_id]["images"][adata.uns["spatial"]["use_quality"]]
+    # Overlay the tissue image
+    a.imshow(image, alpha=tissue_alpha, zorder=-1,)
+
+    if cropped:
+        imagecol = adata.obs["imagecol"]
+        imagerow = adata.obs["imagerow"]
+
+        a.set_xlim(imagecol.min() - margin,
+                imagecol.max() + margin)
+
+        a.set_ylim(imagerow.min() - margin,
+                imagerow.max() + margin)
+        
+        a.set_ylim(a.get_ylim()[::-1])
+
+    if name is None:
+        name = method
+    if output is not None:
+        fig.savefig(output + "/" + name, dpi=plt.figure().dpi,
+                    bbox_inches='tight', pad_inches=0)
+        
+    
+
+
+    plt.show()
+
+
+
+def _gene_plot(adata, method, genes):
+
+    # Gene plot option
+
+    if len(genes) == 0:
+        raise ValueError('Genes shoule be provided, please input genes')
+
+    elif len(genes) == 1:
+
+        if genes[0] not in adata.var.index:
+            raise ValueError(
+                genes[0] + ' is not exist in the data, please try another gene')
+
+        colors = adata[:, genes].to_df().iloc[:, -1]
+
+        return colors
+    else:
+
+        for gene in genes:
+            if gene not in adata.var.index:
+                genes.remove(gene)
+                warnings.warn("We removed " + gene +
+                              " because they not exist in the data")
+
+            if len(genes) == 0:
+                raise ValueError(
+                    'All provided genes are not exist in the data')
+
+        count_gene = adata[:, genes].to_df()
+
+        if method is None:
+            raise ValueError(
+                'Please provide method to combine genes by NaiveMean/NaiveSum/CumSum')
+
+        if method == "NaiveMean":
+            present_genes = (count_gene > 0).sum(axis=1) / len(genes)
+
+            count_gene = (count_gene.mean(axis=1)) * present_genes
+        elif method == "NaiveSum":
+            present_genes = (count_gene > 0).sum(axis=1) / len(genes)
+
+            count_gene = (count_gene.sum(axis=1)) * present_genes
+
+        elif method == "CumSum":
+            count_gene = count_gene.cumsum(axis=1).iloc[:, -1]
+
+        colors = count_gene
+
+        return colors
+
+
+"""Reading and Writing
+"""
+from pathlib import Path, PurePath
+from typing import Optional, Union
+from anndata import AnnData
+import numpy as np
+from PIL import Image
+import pandas as pd
+import stlearn
+import scanpy
+import scipy
+
+def Read10X(
+    path: Union[str, Path],
+    genome: Optional[str] = None,
+    count_file: str = "filtered_feature_bc_matrix.h5",
+    library_id: str = None,
+    load_images: Optional[bool] = True,
+    quality: str = "hires"
+    ) -> AnnData:
+    
+    """\
+    Read Visium data from 10X (wrap read_visium from scanpy)
+
+    In addition to reading regular 10x output,
+    this looks for the `spatial` folder and loads images,
+    coordinates and scale factors.
+    Based on the `Space Ranger output docs`_.
+
+    .. _Space Ranger output docs: https://support.10xgenomics.com/spatial-gene-expression/software/pipelines/latest/output/overview
+
+    Parameters
+    ----------
+    path
+        Path to directory for visium datafiles.
+    genome
+        Filter expression to genes within this genome.
+    count_file
+        Which file in the passed directory to use as the count file. Typically would be one of:
+        'filtered_feature_bc_matrix.h5' or 'raw_feature_bc_matrix.h5'.
+    library_id
+        Identifier for the visium library. Can be modified when concatenating multiple adata objects.
+    load_images
+        Load image or not.
+    quality
+        Set quality that convert to stlearn to use. Store in anndata.obs['imagecol' & 'imagerow']
+
+
+    Returns
+    -------
+    Annotated data matrix, where observations/cells are named by their
+    barcode and variables/genes by gene name. Stores the following information:
+    :attr:`~anndata.AnnData.X`
+        The data matrix is stored
+    :attr:`~anndata.AnnData.obs_names`
+        Cell names
+    :attr:`~anndata.AnnData.var_names`
+        Gene names
+    :attr:`~anndata.AnnData.var`\\ `['gene_ids']`
+        Gene IDs
+    :attr:`~anndata.AnnData.var`\\ `['feature_types']`
+        Feature types
+    :attr:`~anndata.AnnData.uns`\\ `['spatial']`
+        Dict of spaceranger output files with 'library_id' as key
+    :attr:`~anndata.AnnData.uns`\\ `['spatial'][library_id]['images']`
+        Dict of images (`'hires'` and `'lowres'`)
+    :attr:`~anndata.AnnData.uns`\\ `['spatial'][library_id]['scalefactors']`
+        Scale factors for the spots
+    :attr:`~anndata.AnnData.uns`\\ `['spatial'][library_id]['metadata']`
+        Files metadata: 'chemistry_description', 'software_version'
+    :attr:`~anndata.AnnData.obsm`\\ `['spatial']`
+        Spatial spot coordinates, usable as `basis` by :func:`~scanpy.pl.embedding`.
+    """
+    
+    from scanpy import read_visium
+    adata = read_visium(path, genome=None,
+     count_file=count_file,
+      library_id=library_id,
+       load_images=True)
+    adata.var_names_make_unique()
+
+    adata.obs['sum_counts'] = np.array(adata.X.sum(axis=1))
+
+    
+    if library_id is None:
+        library_id = list(adata.uns["spatial"].keys())[0]
+        
+    scale = adata.uns["spatial"][library_id]["scalefactors"]["tissue_"+quality+"_scalef"]
+    image_coor = adata.obsm["spatial"]*scale
+
+    adata.obs["imagecol"] = image_coor[:,0]
+    adata.obs["imagerow"] = image_coor[:,1]
+    adata.uns["spatial"]["use_quality"] = quality
+
+    return adata
+
+
+def ReadOldST(
+    count_matrix_file: Union[str, Path] = None,
+    spatial_file: Union[str, Path] = None,
+    image_file: Union[str, Path] = None,
+    library_id: str = "OldST",
+    scale: float = 1.0,
+    quality: str = "hires"
+    ) -> AnnData:
+
+    """\
+    Read Old Spatial Transcriptomics data
+
+    Parameters
+    ----------
+    count_matrix_file
+        Path to count matrix file.
+    spatial_file
+        Path to spatial location file.
+    image_file
+        Path to the tissue image file
+    library_id
+        Identifier for the visium library. Can be modified when concatenating multiple adata objects.
+    scale
+        Set scale factor.
+    quality
+        Set quality that convert to stlearn to use. Store in anndata.obs['imagecol' & 'imagerow']
+    Returns
+    -------
+    AnnData
+    """
+
+    adata = stlearn.read.file_table(count_matrix_file)
+    adata=stlearn.add.parsing(adata,
+        coordinates_file = spatial_file)
+    stlearn.add.image(adata, library_id=library_id, quality=quality,imgpath=image_file, scale=scale)
+
+    adata.obs['sum_counts'] = np.array(adata.X.sum(axis=1))
+
+    return adata
