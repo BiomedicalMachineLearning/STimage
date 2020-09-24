c82b05111544a3e83413093c13256b3d33731b95
uqxtan9 <uqxtan9@wiener.hpc.dc.uq.edu.au>
2020-09-24 23:39:33 +1000

    add cnn_nb

--- stimage/utils.py
+++ stimage/utils.py
@@ -329,3 +329,92 @@ def ReadOldST(
     adata.obs['sum_counts'] = np.array(adata.X.sum(axis=1))
 
     return adata
+
+
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras.layers import Input, Dense, Lambda, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.losses import BinaryCrossentropy
+
+
+def negative_binomial_layer(x):
+    """
+    Lambda function for generating negative binomial parameters
+    n and p from a Dense(2) output.
+    Assumes tensorflow 2 backend.
+    
+    Usage
+    -----
+    outputs = Dense(2)(final_layer)
+    distribution_outputs = Lambda(negative_binomial_layer)(outputs)
+    
+    Parameters
+    ----------
+    x : tf.Tensor
+        output tensor of Dense layer
+        
+    Returns
+    -------
+    out_tensor : tf.Tensor
+        
+    """
+    
+    # Get the number of dimensions of the input
+    num_dims = len(x.get_shape())
+    
+    # Separate the parameters
+    n, p = tf.unstack(x, num=2, axis=-1)
+    
+    # Add one dimension to make the right shape
+    n = tf.expand_dims(n, -1)
+    p = tf.expand_dims(p, -1)
+        
+    # Apply a softplus to make positive
+    n = tf.keras.activations.softplus(n)
+    
+    # Apply a sigmoid activation to bound between 0 and 1
+    p = tf.keras.activations.sigmoid(p)
+
+    # Join back together again
+    out_tensor = tf.concat((n, p), axis=num_dims-1)
+
+    return out_tensor
+
+
+def negative_binomial_loss(y_true, y_pred):
+    """
+    Negative binomial loss function.
+    Assumes tensorflow backend.
+    
+    Parameters
+    ----------
+    y_true : tf.Tensor
+        Ground truth values of predicted variable.
+    y_pred : tf.Tensor
+        n and p values of predicted distribution.
+        
+    Returns
+    -------
+    nll : tf.Tensor
+        Negative log likelihood.
+    """
+
+    # Separate the parameters
+    n, p = tf.unstack(y_pred, num=2, axis=-1)
+    
+    # Add one dimension to make the right shape
+    n = tf.expand_dims(n, -1)
+    p = tf.expand_dims(p, -1)
+    
+    # Calculate the negative log likelihood
+    nll = (
+        tf.math.lgamma(n) 
+        + tf.math.lgamma(y_true + 1)
+        - tf.math.lgamma(n + y_true)
+        - n * tf.math.log(p)
+        - y_true * tf.math.log(1 - p)
+    )                  
+
+    return nll
