f30677aa8bc3913781e41f78a718269856c5c468
uqxtan9 <uqxtan9@wiener.hpc.dc.uq.edu.au>
2021-08-10 13:03:18 +1000

    add more cnn base

--- stimage/02_Training.py
+++ stimage/02_Training.py
@@ -6,10 +6,9 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from anndata import read_h5ad
-
 from _data_generator import DataGenerator
-from _model import CNN_NB_multiple_genes, PrinterCallback, CNN_linear_model
+from _model import CNN_NB_multiple_genes, PrinterCallback
+from anndata import read_h5ad
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="STimage software --- Training")
@@ -20,59 +19,72 @@ if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read(args.config)
 
-    model_name = config["TRAINING"]["model_name"]
     tile_size = int(config["DATASET"]["tile_size"])
     gene_selection = config["DATASET"]["gene_selection"]
     training_ratio = float(config["DATASET"]["training_ratio"])
     valid_ratio = float(config["DATASET"]["valid_ratio"])
+
+    model_name = config["TRAINING"]["model_name"]
     batch_size = int(config["TRAINING"]["batch_size"])
     early_stop = config["TRAINING"].getboolean("early_stop")
     epochs = int(config["TRAINING"]["epochs"])
+    cnn_base = config["TRAINING"]["cnn_base"]
+    fine_tuning = config["TRAINING"].getboolean("fine_tuning")
+
     save_train_history = config["RESULTS"].getboolean("save_train_history")
     save_model_weights = config["RESULTS"].getboolean("save_model_weights")
+
     OUT_PATH = Path(config["PATH"]["OUT_PATH"])
     DATA_PATH = Path(config["PATH"]["DATA_PATH"])
     OUT_PATH.mkdir(parents=True, exist_ok=True)
 
-    adata_all = read_h5ad(DATA_PATH / "all_adata.h5ad")
-    if gene_selection == "tumour":
-        comm_genes = ["PABPC1", "GNAS", "HSP90AB1", "TFF3",
-                      "ATP1A1", "COX6C", "B2M", "FASN",
-                      "ACTG1", "HLA-B"]
-
-    if gene_selection == "markers":
-        comm_genes = ["SLITRK6", "PGM5", "LINC00645",
-                      "TTLL12", "COX6C", "CPB1",
-                      "KRT5", "MALAT1"]
-
-    if gene_selection == "top250":
-        adata_all.var["mean_expression"] = np.mean(adata_all.X, axis=0)
-        comm_genes = adata_all.var["mean_expression"].sort_values(ascending=False
-                                                                  ).index[0:250]
-
-    adata_all = adata_all[:, comm_genes].copy()
-    if model_name.split("_")[-1] == "classification":
-        adata_all.X = adata_all.to_df().apply(lambda x: pd.qcut(x, 3, duplicates='drop', labels=False))
-    All_sample = pd.Series(adata_all.obs["library_id"].unique())
-    training_valid_sample = All_sample.sample(frac=training_ratio, random_state=1)
-    training_valid_index_ = All_sample.index.isin(training_valid_sample.index)
-
-    if len(training_valid_sample) > 1:
-        valid_sample = training_valid_sample.sample(frac=valid_ratio, random_state=1)
-        valid_index_ = training_valid_sample.index.isin(valid_sample.index)
-        training_sample = training_valid_sample[~valid_index_]
+    if (DATA_PATH / "train_adata.h5ad").exists() and \
+            (DATA_PATH / "valid_adata.h5ad").exists() and \
+            (DATA_PATH / "test_adata.h5ad").exists():
+        train_adata = read_h5ad(DATA_PATH / "train_adata.h5ad")
+        valid_adata = read_h5ad(DATA_PATH / "valid_adata.h5ad")
+        test_adata = read_h5ad(DATA_PATH / "test_adata.h5ad")
+        comm_genes = train_adata.var_names
     else:
-        valid_sample = training_valid_sample
-        training_sample = training_valid_sample
+        adata_all = read_h5ad(DATA_PATH / "all_adata.h5ad")
+        if gene_selection == "tumour":
+            comm_genes = ["PABPC1", "GNAS", "HSP90AB1", "TFF3",
+                          "ATP1A1", "COX6C", "B2M", "FASN",
+                          "ACTG1", "HLA-B"]
 
-    test_Sample = All_sample[~training_valid_index_].copy()
+        if gene_selection == "markers":
+            comm_genes = ["SLITRK6", "PGM5", "LINC00645",
+                          "TTLL12", "COX6C", "CPB1",
+                          "KRT5", "MALAT1"]
 
-    train_adata = adata_all[adata_all.obs["library_id"].isin(training_sample)].copy()
-    train_adata.write(DATA_PATH / "train_adata.h5ad")
-    valid_adata = adata_all[adata_all.obs["library_id"].isin(valid_sample)].copy()
-    valid_adata.write(DATA_PATH / "valid_adata.h5ad")
-    test_adata = adata_all[adata_all.obs["library_id"].isin(test_Sample)].copy()
-    test_adata.write(DATA_PATH / "test_adata.h5ad")
+        if gene_selection == "top250":
+            adata_all.var["mean_expression"] = np.mean(adata_all.X, axis=0)
+            comm_genes = adata_all.var["mean_expression"].sort_values(ascending=False
+                                                                      ).index[0:250]
+
+        adata_all = adata_all[:, comm_genes].copy()
+        if model_name.split("_")[-1] == "classification":
+            adata_all.X = adata_all.to_df().apply(lambda x: pd.qcut(x, 3, duplicates='drop', labels=False))
+        All_sample = pd.Series(adata_all.obs["library_id"].unique())
+        training_valid_sample = All_sample.sample(frac=training_ratio, random_state=1)
+        training_valid_index_ = All_sample.index.isin(training_valid_sample.index)
+
+        if len(training_valid_sample) > 1:
+            valid_sample = training_valid_sample.sample(frac=valid_ratio, random_state=1)
+            valid_index_ = training_valid_sample.index.isin(valid_sample.index)
+            training_sample = training_valid_sample[~valid_index_]
+        else:
+            valid_sample = training_valid_sample
+            training_sample = training_valid_sample
+
+        test_Sample = All_sample[~training_valid_index_].copy()
+
+        train_adata = adata_all[adata_all.obs["library_id"].isin(training_sample)].copy()
+        train_adata.write(DATA_PATH / "train_adata.h5ad")
+        valid_adata = adata_all[adata_all.obs["library_id"].isin(valid_sample)].copy()
+        valid_adata.write(DATA_PATH / "valid_adata.h5ad")
+        test_adata = adata_all[adata_all.obs["library_id"].isin(test_Sample)].copy()
+        test_adata.write(DATA_PATH / "test_adata.h5ad")
 
     n_genes = len(comm_genes)
     train_gen = tf.data.Dataset.from_generator(
@@ -95,7 +107,7 @@ if __name__ == "__main__":
 
     model = None
     if model_name == "NB_regression":
-        model = CNN_NB_multiple_genes((tile_size, tile_size, 3), n_genes)
+        model = CNN_NB_multiple_genes((tile_size, tile_size, 3), n_genes, cnnbase=cnn_base, ft=fine_tuning)
 
     callbacks = [PrinterCallback()]
     # callbacks = []
